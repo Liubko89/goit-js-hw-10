0f8a27db4ead2438dfa364a6786acd6e2bec7185
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-01-13 00:46:52 +0000

    Deploying to gh-pages from @ Liubko89/goit-js-hw-10@00620fa2485d42b45a78d3d33478159a9a68fb77 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";import"./assets/vendor-651d7991.js";const t=document.querySelector(".form");t.addEventListener("submit",o);function o(e){e.preventDefault(),console.log(e.target.elements.state.value),console.log(e.target.elements.delay.value)}
+//# sourceMappingURL=commonHelpers2.js.map
